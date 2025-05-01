cadab6f1247d323965d92feb47c5ccdcef90cdbf
Philskillz <philipp.nikos@gmail.com>
2025-05-01 14:03:51 +0200

    Updated host bind to 0.0.0.0

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ async def page_not_found(e):
     return await send_from_directory('src', '404.html')
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
