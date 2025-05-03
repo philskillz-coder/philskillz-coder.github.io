a9c0e478cac4576bc0eba5b7c7d7de3bc174d0e4
Philskillz <philipp.nikos@gmail.com>
2025-05-03 19:05:00 +0200

    Fix fix

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ app = Quart(__name__)
 
 @app.route('/')
 @app.route('/<path:path>')
-async def serve_static(path='index'):
-    return await send_from_directory('src', path+".html")
+async def serve_static(path='index.html'):
+    if not path.endswith('.html'):
+        path += '.html'
+    return await send_from_directory('src', path)
 
 # 404 page
 @app.errorhandler(404)
